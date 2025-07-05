87f0b5f39d565314c1b5889f5c6b0c23a2a268a7
Ana Carolina Claro <pwclaroanacarolina@gmail.com>
2025-07-05 10:34:27 -0300

    Update ContactsController.java

--- src/main/java/contact/list/contacts/controllers/ContactsController.java
+++ src/main/java/contact/list/contacts/controllers/ContactsController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import contact.list.contacts.entities.Contacts;
 import contact.list.contacts.service.ContactsService;
 
-@CrossOrigin(origins = "http://localhost:4200") 
+@CrossOrigin(origins = "https://contactslist.duckdns.org/contacts") 
 @RestController
 @RequestMapping("/contacts")
 public class ContactsController {
